d5f78ad442347f006260f5faf986cb9654ade6f6
Ahmedmwinyi <Madiylal@gmail.com>
2026-03-28 09:49:46 +0300

    feat(auth): implement user authentication and profile retrieval
    - Added `getMe` and `authCallback` functions in `authController.ts` for user profile retrieval and authentication callback handling.
    - Created `protectedRoute` middleware in `auth.ts` to secure routes and fetch user details.
    - Introduced `errorHandler` middleware for centralized error handling.
    - Updated `authRoutes.ts` to include new routes for user profile and authentication callback.
    - Added global type definitions for Clerk in `globals.d.ts`.
    - Updated `.gitignore` to exclude `.env` files.
    - Added Clerk dependencies in `bun.lock` and `package.json`.
    - Refactored `App.jsx` to integrate Clerk's authentication UI components.
    - Wrapped the application in `ClerkProvider` in `main.jsx` to provide authentication context.
    
    Signed-off-by: Ahmedmwinyi <Madiylal@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import { clerkMiddleware } from '@clerk/express'
+
 import authRoutes from "./routes/authRoutes"
 import chatRoutes from "./routes/chatRoutes"
 import messageRoutes from "./routes/messageRoutes"
 import userRoutes from "./routes/userRoutes"
+import { errorHandler } from "./middleware/errorHandler";
+
 
 const app = express();
 
 app.use(express.json())
+
+app.use(clerkMiddleware())
 
 app.get("/health", (req, res) => {
   res.json({ status: "Ok", message: "Server is up and running!" });
@@ -16,5 +22,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/chat", chatRoutes)
 app.use("/api/message", messageRoutes)
 app.use("/api/user", userRoutes)
+
+app.use(errorHandler)
 
 export default app;
