d382d9b71464e38c7e4a980c5212e22243729a76
Ahmedmwinyi <Madiylal@gmail.com>
2026-03-30 13:56:57 +0300

    feat(docker): add Dockerfile for application containerization
    fix(app): format imports and improve code consistency
    
    Signed-off-by: Ahmedmwinyi <Madiylal@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,36 @@
 import express from "express";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import path from "path";
 
-import authRoutes from "./routes/authRoutes"
-import chatRoutes from "./routes/chatRoutes"
-import messageRoutes from "./routes/messageRoutes"
-import userRoutes from "./routes/userRoutes"
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
 import { errorHandler } from "./middleware/errorHandler";
-
 
 const app = express();
 
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
-app.use(express.json())
-
+app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.json({ status: "Ok", message: "Server is up and running!" });
 });
 
-app.use("/api/auth", authRoutes)
-app.use("/api/chat", chatRoutes)
-app.use("/api/message", messageRoutes)
-app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("*any", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 
 export default app;
