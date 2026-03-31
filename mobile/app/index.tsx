9db7f0954e2f3f516096d8fbbeaf34ffefc48a91
Ahmedmwinyi <Madiylal@gmail.com>
2026-03-31 10:54:06 +0300

    refactor: replace inline styles with Tailwind CSS classes for consistency
    Signed-off-by: Ahmedmwinyi <Madiylal@gmail.com>

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,12 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+      className="flex-1 items-center justify-center bg-white">
       <Text className="text-red-700 text-2xl">Hello.</Text>
     </View>
   );
