9e7ab2765d81f4506e0ff3d89f1c2d4e3937db4c
Ahmedmwinyi <Madiylal@gmail.com>
2026-04-03 12:46:27 +0300

    feat: integrate NativeWind and Tailwind CSS for styling
    - Added global CSS file for Tailwind directives.
    - Configured Metro bundler to use NativeWind with the new CSS file.
    - Created TypeScript declaration file for NativeWind.
    - Updated package.json to include NativeWind and Tailwind CSS dependencies.
    - Added Tailwind configuration file to specify content paths and presets.
    - Updated TypeScript configuration to include NativeWind types.
    
    Signed-off-by: Ahmedmwinyi <Madiylal@gmail.com>

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-3xl text-green-600 bg-blue-700">Janu</Text>
     </View>
   );
 }
